6bbed331ea8a2f7c05709b5daa016a0c9133be33
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2024-02-20 19:25:16 -0500

    Initial Commit

--- Lab1.cpp
+++ Lab1.cpp
@@ -9,21 +9,7 @@ void change_ranking();
 
 int main() {
     vector<string> movies { "The Incredibles", "Monsters Inc.", "Toy Story", "Cars", "Finding Nemo" };
-    cout << "The Movie Rankings program\n";
-    display_rankings(movies);
-    char choice = get_choice();
 
-    while (tolower(choice) == 'y') {
-        int current_ranking = 0;
-        cout << "Enter current ranking of a movie: ";
-        cin >> current_ranking;
-        int new_ranking = 0;
-        cout << "Enter new ranking of the movie: ";
-        cin >> new_ranking;
-        change_ranking(movies, current_ranking, new_ranking);
-        display_rankings(movies); // show updated rankings
-        choice = get_choice(); // ask if user wants to continue
-        }
 }
 
 void display_rankings() {
